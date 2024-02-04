674ad484e4d5ffd6fe2eb6ef396de302eeb3a2cc
NotVndtta <thenotalexey192@gmail.com>
2024-02-05 01:26:24 +0300

    Start button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
+    <div className='max-w-860 mx-auto my-10'>
+        <h1 className="text-3xl font-bold underline my-2">    
+          Memory Game
         </h1>
+        <button className='bg-transparent border-2 border-white py-2 px-4 rounded font-bold text-white cursor-pointer text-lg
+         hover:bg-pink-600 hover:text-white'>
+          New Game</button>
+    </div>
   );
 }
 
