870a7d6b479cf962858bbe60b272b4ad0442a891
NotVndtta <thenotalexey192@gmail.com>
2024-02-12 01:22:06 +0300

    Complete functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Card from './components/Card';
 
 const cardImages = [
@@ -15,12 +15,16 @@ function App() {
   const [turns, setTurns] = useState(0) // Объявляем состояние для количества ходов и финкции для его обновления
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+  const [gameOver, setGameOver] = useState(false);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages] // создание массива, который сожержит увдоенный массив картинок
     .sort(() => Math.random() - 0.5) // перемешивание элементов
     .map((card) => ({...card, id: Math.random() })) // присваивание каждому элементу массива случйного идентификатора
-  
+    
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards) // обновление состояния новым перемешанным массивом карточек
     setTurns(0)
   }
@@ -28,15 +32,26 @@ function App() {
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
+  const resetTurn = useCallback(() => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+
+    if (turns >= 14) {  
+      setGameOver(true);  
+    }
+  }, [turns]);
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true)
 
       if(choiceOne.src === choiceTwo.src) {
-        setCards(prevCards => {
+        setCards(prevCards => { 
           return prevCards.map(card => {
             if (card.src === choiceOne.src){
-              return {...card, matched: true}
+              return {...card, matched: true} // обновление состояния карт
             } else {
               return card
             }
@@ -44,16 +59,24 @@ function App() {
         })
         resetTurn()
       } else {
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
     }
-  }, [choiceOne, choiceTwo])
+  }, [choiceOne, choiceTwo, resetTurn])
 
-  const resetTurn = () => {
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(prevTurns => prevTurns + 1)
-  }
+  
+
+  useEffect(() => { 
+    shuffleCards() 
+  }, []) 
+
+  const renderGameOverMessage = () => {  
+    if (gameOver) {  
+      return (  
+        <p>{turns < 15 ? "Grats! " : "Oh you lose"}</p>  
+      )  
+    }  
+  }  
 
   return (
     <div className='max-w-[860px] mx-auto my-10'>
@@ -63,11 +86,16 @@ function App() {
         <button onClick={shuffleCards} className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold text-white cursor-pointer text-lg
          hover:bg-pink-600 hover:text-white'>
           New Game</button>
+            <p>Turns: {turns}</p>
+          {renderGameOverMessage()}
           <div className='mt-10 grid grid-cols-4 gap-5'>
           {cards.map(card => (
-            <Card  key={card.id} card={card} handleChoice = {handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched}/>  
+            <Card  key={card.id} card={card} handleChoice = {handleChoice} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled= {disabled} 
+            /> 
             ))}
-
+          
           </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-
-const Card = ({card, handleChoice, flipped}) => {
+const Card = ({card, handleChoice, flipped, disabled}) => {
 
 const handleClick = () => {
-  handleChoice(card)
+  if (!disabled) {
+    handleChoice(card)
+  }
 }
 
-  return (
-    <div className='relative'>
-        <div className=''>
-        <img className= {`${flipped ? "rotate-90 absolute" : "rotate-0"} w-full block border-2 border-white`} src={card.src} alt="front" />
-        <img className='w-full block border-2 border-white' src= {process.env.PUBLIC_URL + '/img/cover.png'} onClick={handleClick} alt="back" />
-        </div>
+return (
+    <div className = "relative">
+    <div className= {flipped? "transition-all ease-in duration-200 delay-200": ""}>
+       <img className="w-full block border-2 border-white rounded-md" src={card.src} alt= "card front" />
     </div>
+    <div className= {flipped? "absolute top-0 transition-all ease-in duration-200 delay-200 transform scale-x-0": "absolute top-0"}>
+       <img className="w-full block border-2 border-white rounded-md" src={process.env.PUBLIC_URL + "/img/cover.png"} 
+       onClick={handleClick}
+       alt = "card back" />
+   </div>
+</div> 
   )
 }
 
