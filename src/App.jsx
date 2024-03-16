ad45875d23efefaa112542e71944b6e3f059d765
NotVndtta <thenotalexey192@gmail.com>
2024-03-17 00:22:09 +0300

    game mode

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ const cardImages = [
   {"src": process.env.PUBLIC_URL + '/img/youtube.png', matched: false},
   {"src": process.env.PUBLIC_URL + '/img/figma.png', matched: false},
   {"src": process.env.PUBLIC_URL + '/img/telegram.png', matched: false},
+  {"src": process.env.PUBLIC_URL + '/img/twitter.png', matched: false},
+  {"src": process.env.PUBLIC_URL + '/img/google.png', matched: false},
   
 ]
 
@@ -24,6 +26,28 @@ function App() {
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
+
+  const [cardSize, setCardSize] = useState({width: '160px', height: '160px'});
+  const [gridSize, setGridSize] = useState({rows: 3, cols: 3});
+  
+  const setGridSize2x4 = () => {
+    setGridSize({rows: 2, cols: 4});
+    setCardSize({width: '160px', height: '160px'});
+    shuffleCards();
+
+  }
+
+  const setGridSize3x4 = () => {
+    setGridSize({rows: 3, cols: 4});
+    setCardSize({width: '160px', height: '160px'});
+    shuffleCards();
+  }
+  
+  const setGridSize4x4 = () => {
+    setGridSize({rows: 4, cols: 4});
+    setCardSize({width: '140px', height: '140px'});
+    shuffleCards();
+  }
 
   const customStyles = {
     content: {
@@ -41,17 +65,17 @@ function App() {
     },
   };
 
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages] // создание массива, который сожержит увдоенный массив картинок
-    .sort(() => Math.random() - 0.5) // перемешивание элементов
-    .map((card) => ({...card, id: Math.random() })) // присваивание каждому элементу массива случйного идентификатора
-    
+  const shuffleCards = useCallback(() => {
+    const shuffledCards = [...cardImages.slice(0, (gridSize.rows * gridSize.cols) / 2), ...cardImages.slice(0, (gridSize.rows * gridSize.cols) / 2)]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random() }))
+  
     setChoiceOne(null)
     setChoiceTwo(null)
-    setCards(shuffledCards) // обновление состояния новым перемешанным массивом карточек
+    setCards(shuffledCards)
     setTurns(0)
     setGameOver(false)
-  }
+  }, [gridSize]);
 
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
@@ -89,17 +113,16 @@ function App() {
   }, [choiceOne, choiceTwo, resetTurn])
 
   
-
   useEffect(() => { 
     shuffleCards() 
-  }, []) 
+  }, [shuffleCards, gridSize]) 
 
   
   useEffect(() => {
     const gameStarted = turns > 0;
     const allCardsMatched = cards.every(card => card.matched);
     if (gameStarted && allCardsMatched) {
-      setModalMessage("Поздравляем! Вы выиграли!");
+      setModalMessage("Grats! You win!");
       setModalIsOpen(true);
     }
   }, [cards, turns]);
@@ -107,7 +130,7 @@ function App() {
 
   useEffect(() => {
     if (gameOver) {
-      setModalMessage(turns < 15 ? "Поздравляем! Вы выиграли!" : "О нет! Вы проиграли!");
+      setModalMessage(turns < 15 ? "Grats! You win!" : "Oh no! You lose :(");
       setModalIsOpen(true);
     }
   }, [gameOver, turns]);
@@ -120,23 +143,43 @@ function App() {
 
 
   return (
-    <div className='max-w-[700px] mx-auto my-10'>
+    <div className='max-w-[680px] mx-auto my-10'>
         <h1 className="text-3xl font-bold  my-2">    
           Memory Game
         </h1>
-        <button onClick={shuffleCards} className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold 
+        <button onClick={shuffleCards} 
+          className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold 
         text-white cursor-pointer text-lg
          hover:bg-pink-600 hover:text-white'>
           New Game</button>
+        <button onClick={setGridSize2x4} 
+          className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold 
+          text-white cursor-pointer text-lg
+          hover:bg-pink-600 hover:text-white ml-4'>
+          Set Grid 2x4
+        </button>
+        <button onClick={setGridSize3x4} 
+          className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold 
+          text-white cursor-pointer text-lg
+          hover:bg-pink-600 hover:text-white ml-4'>
+          Set Grid 3x4</button>
+        <button onClick={setGridSize4x4} 
+          className='bg-transparent border-2 border-white py-1 px-3 rounded font-bold 
+          text-white cursor-pointer text-lg
+          hover:bg-pink-600 hover:text-white ml-4'>
+          Set Grid 4x4
+        </button>
             <p>Turns: {turns}</p>
           
           <div className='mt-10 grid grid-cols-4 gap-5'>
           {cards.map(card => (
-            <Card  key={card.id} card={card} handleChoice = {handleChoice} 
+            <Card key={`${card.id}-${cardSize.width}-${cardSize.height}`} 
+            card={card} handleChoice = {handleChoice} 
             flipped={card === choiceOne || card === choiceTwo || card.matched}
             disabled= {disabled} 
             gameOver={gameOver}
-
+            cardSize={cardSize}
+            
             /> 
             ))}
           <Modal
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-const Card = ({card, handleChoice, flipped, disabled, gameOver}) => {
+const Card = ({card, handleChoice, flipped, disabled, gameOver,cardSize}) => {
 
 const handleClick = () => {
   if (!disabled && !gameOver) {
@@ -10,10 +10,14 @@ const handleClick = () => {
 return (
     <div className = "relative m-0">
     <div className= {flipped? "transition-all ease-in duration-200 delay-200": ""}>
-       <img className="block border-2 border-white rounded-md max-w-[160px] max-h-[160px]" src={card.src} alt= "card front" />
+       <img className="block border-2 border-white rounded-md "
+       style={{width: cardSize.width, height: cardSize.height}} 
+       src={card.src} alt= "card front" />
     </div>
     <div className= {flipped? "absolute top-0 transition-all ease-in duration-200 delay-200 transform scale-x-0": "absolute top-0"}>
-       <img className=" block border-2 border-white rounded-md max-w-[160px] max-h-[160px]" src={process.env.PUBLIC_URL + "/img/cover.png"} 
+       <img className="block border-2 border-white rounded-md"
+       style={{width: cardSize.width, height: cardSize.height}}
+       src={process.env.PUBLIC_URL + "/img/cover.png"} 
        onClick={handleClick}
        alt = "card back" />
    </div>
